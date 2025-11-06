d8ed2ff146b37037e41966e09b5284821a40dacf
Rahiq Rahman <rahiqrahman2001@gmail.com>
2025-11-06 19:42:21 +0600

    navbar added

--- main_web/views.py
+++ main_web/views.py
@@ -3,5 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 
 def homepage(request):
-    
     return render(request, 'home.html')
+
+def event_details(request):
+    return render(request, 'event-details.html')
+
+def organizers(request):
+    return render(request, 'organizers.html')
+
+def timeline(request):
+    return render(request, 'timeline.html')
